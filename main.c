0e1a054b31d9a663c62e168275d94971311ef324
Ben Newman <benjamin.newman@uon.edu.au>
2019-04-03 02:02:45 +0000

    thought bout user entry and choosing entry

--- main.c
+++ main.c
@@ -6,7 +6,24 @@ int decript(int d);
 
 int main() 
   {
-      
+      char select;
+      printf("Please enter a selection\n"); //user choice as task
+      printf("Press 'a' to run task 1\n");
+      printf("Press 'b' to run task 2\n");
+      printf("Press 'c' to run task 3\n");
+      printf("Press 'd' to run task 4\n");
+      printf("Press 'd' to run task 5\n");
+      printf("Press 'd' to run task 6\n");
+      scanf("%c", &select)
+      switch(select) { // run the program selected
+          case a:
+          case b:
+          case c:
+          case d:
+          case e:
+          case f:
+          default: printf("Incorrect entry please try again");
+      }
   return O;
   }
 
