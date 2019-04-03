cbe9a80995c198f1c2816c7aefc1104bf1565a1a
Ben Newman <benjamin.newman@uon.edu.au>
2019-04-03 01:46:50 +0000

    First attempt

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+int encript(int e);
+
+int decript(int d);
+
 int main() 
   {
+      
   return O;
   }
+
+int encript(int x) {
+    int m, key, e;
+    e=(m+k)(%k);
+    return e;
+}
+
+int decript(int y) {
+    int c, key, d;
+    d=(c-k)*(%*26);
+    return d;
+}
