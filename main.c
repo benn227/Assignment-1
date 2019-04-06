58a94e39d7362cfda19ccd1c586e0075ab8a499f
Ben Newman <benjamin.newman@uon.edu.au>
2019-04-06 04:32:22 +0000

    started again

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 
 int main() 
   {
-      char select;
+      char select=100;
+      
       do {
       printf("Please enter a selection\n"); //user choice as task
       printf("Press 'a' to run task 1\n");
@@ -12,36 +13,12 @@ int main()
       printf("Press 'd' to run task 4\n");
       printf("Press 'e' to run task 5\n");
       printf("Press 'f' to run task 6\n");
-      scanf("%c", &select)
-  } while (select<96, select>102)
-
-      switch(select) // run the program selected
-      { 
-          case a:
-          int m, key, e;
-          e=(m+k)(%k);
-          return e;
-          printf("%c", select);
-          break;
-      
-          case b:
-          int c, key, d;
-          d=(c-k)*(%*26);
-          return d;
-          break;
-          
-          case c:
-          break;
-          case d:
-          break;
-          case e:
-          break;
-          case f:
-          break;
-          default:
-          printf("Error");
-          return 0;
-      }
-  return O;
+      scanf("%c", &select);
   }
 
+      while (select<96 || select>102);
+
+
+printf("%c", select);
+return 0;
+}
