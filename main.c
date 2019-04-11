82c75402c361716418e7f318fb1655d5f4eec7eb
Ben Newman <benjamin.newman@uon.edu.au>
2019-04-11 06:52:08 +0000

    lab end with scanf running

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ void caesarDecrypt(char *decryptMessage);
 
 int main() 
   {
-      char select=98;
+      char select;
       
       
       do {
@@ -17,7 +17,7 @@ int main()
       printf("Press 'd' to run task 4\n");
       printf("Press 'e' to run task 5\n");
       printf("Press 'f' to run task 6\n");
-      //scanf("%c", &select);
+      scanf("%c", &select);
          }
 
       while (select<97 || select>102);
@@ -31,14 +31,15 @@ printf("Selected case %d\n", select);
 switch(select) {
     
     case 1:
-      printf("Case 1 is running\n");
-      char encryptMessage[]="TEST plus OTHER $#@T";
+      
+      printf("The encrypted message is: ");
+      char encryptMessage[]="MON MOTHMA: THE DATA BROUGHT TO US BY THE BOTHAN SPIES PINPOINTS THE EXACT LOCATION OF THE EMPEROR'S NEW BATTLE STATION. WE ALSO KNOW THAT THE WEAPON SYSTEMS OF THIS DEATH STAR ARE NOT YET OPERATIONAL. WITH THE IMPERIAL FLEET SPREAD THROUGHOUT THE GALAXY IN A VAIN EFFORT TO ENGAGE US, IT IS RELATIVELY UNPROTECTED. BUT MOST IMPORTANT OF ALL, WE'VE LEARNED THAT THE EMPEROR HIMSELF IS PERSONALLY OVERSEEING THE FINAL STAGES OF THE CONSTRUCTION OF THIS DEATH STAR. MANY BOTHANS DIED TO BRING US THIS INFORMATION";
       caesarEncrypt(encryptMessage);  
       break;    
       
     case 2:
     
-      printf("Case 2 is running\n");
+      printf("The decrypted message is: ");
       char decryptMessage[]="TVU TVAOTH: AOL KHAH IYVBNOA AV BZ IF AOL IVAOHU ZWPLZ WPUWVPUAZ AOL LEHJA SVJHAPVU VM AOL LTWLYVY'Z ULD IHAASL ZAHAPVU. DL HSZV RUVD AOHA AOL DLHWVU ZFZALTZ VM AOPZ KLHAO ZAHY HYL UVA FLA VWLYHAPVUHS. DPAO AOL PTWLYPHS MSLLA ZWYLHK AOYVBNOVBA AOL NHSHEF PU H CHPU LMMVYA AV LUNHNL BZ, PA PZ YLSHAPCLSF BUWYVALJALK. IBA TVZA PTWVYAHUA VM HSS, DL'CL SLHYULK AOHA AOL LTWLYVY OPTZLSM PZ WLYZVUHSSF VCLYZLLPUN AOL MPUHS ZAHNLZ VM AOL JVUZAYBJAPVU VM AOPZ KLHAO ZAHY. THUF IVAOHUZ KPLK AV IYPUN BZ AOPZ PUMVYTHAPVU";
       caesarDecrypt(decryptMessage);
       break;
@@ -71,7 +72,7 @@ return 0;
 
 void caesarEncrypt(char *encryptMessage) 
 {
-      int i, key =1;
+      int i, key =7;
       for (i=0;encryptMessage[i]!='\0';i++) 
       {   
         if (encryptMessage[i]<65||encryptMessage[i]>90 )
